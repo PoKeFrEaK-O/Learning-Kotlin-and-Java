c48a682cbd684e192d73bf800abeccf7b793a65a
Oliver Fobian <oliver.fobian@check24.de>
2021-02-12 13:03:11 +0100

    Added VariablesAndConstants.java and VariablesAndConstants.kt content.

--- src/main/kotlin/de/check24/learningKotlinJava/java/VariablesAndConstants.java
+++ src/main/kotlin/de/check24/learningKotlinJava/java/VariablesAndConstants.java
@@ -12,12 +12,12 @@ public class VariablesAndConstants {
         short myNumShort = 55;       // Size: 2 bytes, Stores whole numbers from -32,768 to 32,767
         int myNumInt = 5;            // Size: 4 bytes, Stores whole numbers from -2,147,483,648 to 2,147,483,647
         long myNumLong = 12333;      // Size: 8 bytes, Stores whole numbers from -9,223,372,036,854,775,808
-        // to 9,223,372,036,854,775,807
+                                     // to 9,223,372,036,854,775,807
         float myNumFloat = 5.99f;    // Size: 4 bytes, Stores fractional numbers.
-        // Sufficient for storing 6 to 7 decimal digits
+                                     // Sufficient for storing 6 to 7 decimal digits
         double myNumDouble = 76.24d; // Size: 8 bytes, Stores fractional numbers.
-        // Sufficient for storing 15 decimal digits
-        boolean myBool = true;       // Size: 1 bit, Stores true or false values
+                                     // Sufficient for storing 15 decimal digits
+        boolean myBool = true;
         char myLetter = 'D';         // Size: 2 bytes, Stores a single character/letter or ASCII
 
         // Non-Primitive Data Types:
