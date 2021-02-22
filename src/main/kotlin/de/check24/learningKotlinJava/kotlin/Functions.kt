363eeea542a1cd29995e096e3ef76f6aed0d7295
Oliver Fobian <oliver.fobian@check24.de>
2021-02-22 17:54:42 +0100

    Added Functions.java and Functions.kt content.

---
+++ src/main/kotlin/de/check24/learningKotlinJava/kotlin/Functions.kt
@@ -0,0 +1,19 @@
+package de.check24.learningKotlinJava.kotlin
+
+fun main() {
+    fun greet(name: String, day: String): String {
+        return "Hello $name, today is $day."
+    }
+    fun greet2(name: String, day: String) {
+        println("Hello $name, today is $day.")
+    }
+
+    // Aufrufen der Funktion greet
+    println(greet("Bob", "Tuesday"))
+
+    // Aufrufen der Funktion greet2
+    greet2("Max","Monday")
+}
+// Ausgabe:
+// Hello Bob, today is Tuesday.
+// Hello Max, today is Monday.
