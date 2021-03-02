4890a29b80354f096c303f17d025687caed9f7e6
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-02 14:49:06 +0100

    Updated the Relative_complement_of_A_and_B_2.java class and Relative_complement_of_A_and_B_2.kt file.

--- src/main/kotlin/de/check24/learningKotlinJava/kotlin/Relative_complement_of_A_and_B_2.kt
+++ src/main/kotlin/de/check24/learningKotlinJava/kotlin/Relative_complement_of_A_and_B_2.kt
@@ -12,9 +12,13 @@ fun main() {
 
     println("------------------")
     println("Relative complement of a and b: $a")
-    println(a-b)
+
+    // To determine the relative complement in Koltin, you can also use the minus operator:
+    val relativeComp = a-b
+    println("Relative complement of a and b: $relativeComp")
 }
 // Liste A: [a, b, c, d, e]
 // Liste B: [f, e, h, b, e]
 // ------------------
+// Relative complement of a and b: [a, c, d]
 // Relative complement of a and b: [a, c, d]
