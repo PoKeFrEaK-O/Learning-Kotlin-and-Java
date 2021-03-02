4b85dfde7965b73a009d2591be0655dee817a53e
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-02 12:52:31 +0100

    Updated the Combination_of_A_and_B.java class and Combination_of_A_and_B.kt file.

--- src/main/kotlin/de/check24/learningKotlinJava/kotlin/Combination_of_A_and_B.kt
+++ src/main/kotlin/de/check24/learningKotlinJava/kotlin/Combination_of_A_and_B.kt
@@ -4,13 +4,13 @@ fun main() {
     val a = mutableListOf("a", "b", "c", "d", "e")
     val b = mutableListOf("f", "e", "h", "b", "e")
 
-    println("Liste A: $a")
-    println("Liste B: $b")
+    println("Liste a: $a")
+    println("Liste b: $b")
 
     a.addAll(b) // Inserts the elements of list b at the end of list a.
-    println(a)
+    println("Combination of a and b: $a")
 }
 // Ausgabe:
-// Liste A: [a, b, c, d, e]
-// Liste B: [f, e, h, b, e]
-// [a, b, c, d, e, f, e, h, b, e]
+// Liste a: [a, b, c, d, e]
+// Liste b: [f, e, h, b, e]
+// Combination of a and b: [a, b, c, d, e, f, e, h, b, e]
