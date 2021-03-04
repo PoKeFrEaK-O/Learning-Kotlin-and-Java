dbbdc30b3a2daf593a0762e0ed0883e6c19e4f0b
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-04 16:57:27 +0100

    Added Local_functions.java class.

---
+++ src/main/kotlin/de/check24/learningKotlinJava/java/functions/Local_functions.java
@@ -0,0 +1,7 @@
+package de.check24.learningKotlinJava.java.functions;
+
+public class Local_functions {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/kotlin/de/check24/learningKotlinJava/java/functions/Unit_returning.java
@@ -0,0 +1,10 @@
+package de.check24.learningKotlinJava.java.functions;
+
+public class Unit_returning {
+    public static void main(String[] args) {
+
+    }
+    public static void hello(String hello){
+        // return System.out.println("hello");
+    }
+}
