87ec6cf7657b596125069b3de4c4d774f80cf832
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-09 18:30:04 +0100

    Added Declaration.kt file.

---
+++ src/main/kotlin/de/check24/learningKotlinJava/kotlin/classes/Declaration.kt
@@ -0,0 +1,18 @@
+package de.check24.learningKotlinJava.kotlin.classes
+
+// Classes in Kotlin are declared using the keyword class:
+class Classname {
+    // Code of the class Classname
+}
+
+// If the class has no body, curly braces can be omitted:
+class Empty
+
+// Declaring a class with multiple properties:
+class Person(
+        val firstName: String,
+        val lastName: String,
+        var age: Int, // trailing comma
+) {
+    // Code of the class Person
+}
