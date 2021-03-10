4933b4cee874a94b71084e46c35580ccc7d01556
Oliver Fobian <Oliver.Fobian@check24.de>
2021-03-10 17:46:47 +0100

    Added Creating_Instances_of_Classes.kt file.

---
+++ src/main/kotlin/de/check24/learningKotlinJava/kotlin/classes/Creating_Instances_of_Classes.kt
@@ -0,0 +1,19 @@
+package de.check24.learningKotlinJava.kotlin.classes
+
+class Instances (nachname: String, vorname: String){
+    init {
+        println("Der Name der person lautet: $vorname $nachname")
+    }
+}
+
+fun main () {
+    // To create an instance of a class,
+    // call the constructor as if it were a regular function:
+    val instance1 = Instances("Mustermann","Max")
+    val instance2 = Instances("Müller","Markus")
+    instance1
+    instance2
+}
+// Ausgabe:
+// Der Name der person lautet: Max Mustermann
+// Der Name der person lautet: Markus Müller
